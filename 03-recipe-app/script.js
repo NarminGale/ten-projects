f3b479cd3af38e579712321dd12e22b8339feb5d
Narmin A. (VABISS) <narmin.aliyeva@vabiss.com>
2022-04-05 14:49:58 +0400

    Add meal

--- 03-recipe-app/script.js
+++ 03-recipe-app/script.js
@@ -1,10 +1,15 @@
+const meals = document.getElementById('meals')
+
+getRandomMeal()
+
 async function getRandomMeal(){
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     const result = await response.json()
     const randomMeal = result.meals[0]
-}
+    console.log(randomMeal)
 
-getRandomMeal()
+    addMeal(randomMeal, true)
+}
 
 async function getMealById(id){
     const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
@@ -15,3 +20,25 @@ async function getMealsBySearch(name){
     const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + name)
 }
 
+function addMeal(mealData, random = false){
+    const meal = document.createElement('div')
+    meal.classList.add('meal')
+
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${random ? `<span class="random"> Random Recipe </span>` : ""}
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `
+
+    meals.appendChild(meal)
+}
